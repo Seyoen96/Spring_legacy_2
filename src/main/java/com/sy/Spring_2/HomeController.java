59eb12fa7271c6aa98f0c2e56277e419b3090676
Seyoen96 <61647058+Seyoen96@users.noreply.github.com>
2020-04-13 16:38:38 +0900

    add sub

--- src/main/java/com/sy/Spring_2/HomeController.java
+++ src/main/java/com/sy/Spring_2/HomeController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -35,5 +36,25 @@ public class HomeController {
 		
 		return "home";
 	}
+	
+	
+	@RequestMapping(value = "/test",method = RequestMethod.GET)
+	public String test() {
+		System.out.println("test 실행");
+		return "test";
+	}
+	
+	@RequestMapping(value = "/test2",method = RequestMethod.GET)
+	public String test2() {
+		System.out.println("test2 실행");
+		return "sub/test2";
+	}
+	
+	@RequestMapping(value = "/test3",method = RequestMethod.GET)
+	public String test3() {
+		System.out.println("test3실행");
+		return "sub/test3";
+	}
+	
 	
 }
